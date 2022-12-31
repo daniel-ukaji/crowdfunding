b6b84c0fb44433abf6595eb5d55d0b5a8c2a0070
Daniel Ukaji <dannyukaji@gmail.com>
2022-12-31 11:41:03 +0100

    New Commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+import { CustomButton } from './'
+import { logo, menu, search, thirdweb } from '../assets'
+import { navlinks } from '../constants';
+
+const Navbar = () => {
+    const navigate =useNavigate();
+    const [isActive, setIsActive] = useState('dashboard')
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
